31652c417e279c255a3ac22dbaa37be9a5c22107
Dmitry Stroganov <dmitry.stroganov@qligent.com>
2021-07-22 16:59:16 +0300

    fixed Spring test with H2 and flyway

--- ltt/src/test/java/com/lottrading/ltt/LotTradingApplicationTests.java
+++ ltt/src/test/java/com/lottrading/ltt/LotTradingApplicationTests.java
@@ -1,10 +1,30 @@
 package com.lottrading.ltt;
 
+import com.lottrading.ltt.repo.LotRepository;
+import com.lottrading.ltt.service.LotService;
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationInitializer;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureTestDatabase
 class LotTradingApplicationTests {
+
+
+
 
 	@Test
 	void contextLoads() {
